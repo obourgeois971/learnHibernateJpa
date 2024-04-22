696ecae32061987d0d51adb68a20082f81983f41
Olivier Bourgeois <obourgeois@alteca.fr>
2024-04-22 10:19:00 +0200

    Init project

---
+++ src/main/java/com/hibernate4all/tutorial/LearnHibernateJpaApplication.java
@@ -0,0 +1,13 @@
+package com.hibernate4all.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnHibernateJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnHibernateJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hibernate4all/tutorial/LearnHibernateJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hibernate4all.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnHibernateJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
